c5dab5f63e1ee408d185daba9a930ef22753536f
Milton Clark <kingcoder247@gmail.com>
2022-02-25 07:59:16 -0500

    Create transaction get one, able to access individual transaction and get error message if there isn't a transaction at the searched index

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -7,4 +7,17 @@ transactionRoutes.get("/", (req, res) => {
     res.json(transactionsArr);
 });
 
+// transactions/1
+transactionRoutes.get("/:index", (req, res) => {
+    const { index } = req.params;
+    if(transactionsArr[index]){
+        res.json(transactionsArr[index]);
+    } else {
+        res.status(404).json({Error: "Not Found"});
+    }
+});
+
+// transactions
+
+
 module.exports = transactionRoutes;
