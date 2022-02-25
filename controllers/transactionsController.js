95d2058db29e42b8468b3e64edf4bf4bbc99c4d0
Milton Clark <kingcoder247@gmail.com>
2022-02-25 08:20:49 -0500

    Create transactions post and delete, able to post a new transaction and delete an individual transaction from back-end

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -18,6 +18,24 @@ transactionRoutes.get("/:index", (req, res) => {
 });
 
 // transactions
+transactionRoutes.post("/", (req, res) => {
+    transactionsArr.push(req.body);
+    res.json(transactionsArr[transactionsArr.length-1]);
+});
+
+// transactions/1
+transactionRoutes.delete("/:index", (req, res) => {
+    const { index } = req.params;
+    if(transactionsArr[index]){
+        let removed = transactionsArr.splice(index, 1);
+        res.json(removed[0]);
+    } else {
+        res.status(404).json({Error: "Not Found"});
+    }
+});
+
+// /transactions/1
+
 
 
 module.exports = transactionRoutes;
